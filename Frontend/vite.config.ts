d7920872c6f05bcdb2469dc3eac1e9314acdf3ad
AlphaTechini <rehobothokoibu@gmail.com>
2026-02-13 15:35:33 +0100

    added tailwind to fix display

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         sveltekit(),
         nodePolyfills({
             include: ['buffer', 'events']
